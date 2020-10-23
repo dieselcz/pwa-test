9ce874938e2deff150d96a686fedeb20bad3cdd9
Tomas Kratochvil <kratochvil.tomas@bcg.com>
2020-10-23 15:04:17 +0200

    - added activate event for service worker

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-if (!("serviceWorker" in navigator)) {
-    console.log("Service Worker not supported");
-    return;
+if (("serviceWorker" in navigator)) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then(registration => {
+                console.log("SW registered! Scope is: ", registration.scope);
+            })
+            .catch(error => {
+                console.error("Registration failed: ", error);
+            });
+    });
 }
-
-window.addEventListener("load", () => {
-    navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(registration => {
-            console.log("SW registered! Scope is: ", registration.scope);
-        })
-        .catch(error => {
-            console.error("Registration failed: ", error);
-        });
-});
--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,10 @@ self.addEventListener("install", event => {
     );
 });
 
+self.addEventListener("activate", event => {
+    console.log("Service worker activated event!");
+});
+
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request) || fetch(event.request)
