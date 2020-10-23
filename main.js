aea5f8c3a1a33015988ca1647ff2c51d3ec8d9bd
Tomas Kratochvil <kratochvil.tomas@bcg.com>
2020-10-23 15:23:13 +0200

    - rewrite SW regitration to use aync/await

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 if (("serviceWorker" in navigator)) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("/service-worker.js")
-            .then(registration => {
-                console.log("SW registered! Scope is: ", registration.scope);
-            })
-            .catch(error => {
-                console.error("Registration failed: ", error);
-            });
+    window.addEventListener("load", async () => {
+        try {
+            const registration = await navigator.serviceWorker.register("/service-worker.js");
+            console.log("SW registered! Scope is: ", registration.scope);
+        }
+        catch (error) {
+            console.error("Registration failed: ", error);
+        }
     });
 }
